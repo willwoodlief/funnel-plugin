7801673f43c3f0251eeee5e95400a671fa4980f5
willwoodlief <willwoodlief@gmail.com>
2018-06-12 13:03:24 -0500

    email formatting

--- public/ecomhub-fi-user-helper.php
+++ public/ecomhub-fi-user-helper.php
@@ -157,9 +157,7 @@ class EcomhubFiUserHelper
 		    }
 
 		    self::associate_user_data($user_id,'new_password',$password);
-		    wp_mail( $user_email, "Your New Account for Ecomhub.com", "Hello,\nYou have successfully registered for EcomHub.com\n
-		    Here are your credentials\n\nLogin:  $user_email \n Password: $password \n\n
-		     Please to go <a href='https://ecomhub.com/user-dashboard/?user-action=courses' >The User Dashboard</a>" );
+		    wp_mail( $user_email, "Your New Account for Ecomhub.com", "Hello,\nYou have successfully registered for EcomHub.com\n Here are your credentials\n\nLogin:  $user_email \n Password: $password \n\n Please to go <a href='https://ecomhub.com/user-dashboard/?user-action=courses' >The User Dashboard</a>" );
 
 
 		    return ['user_id' =>$user_id,'email'=>$user_email,'password'=>$password];
